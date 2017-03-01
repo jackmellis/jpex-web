6f065002cec929fc50fd1c141bce7ced8481a18b
Jack Ellis <Jack@Administrators-MacBook-Pro.local>
2017-03-01 19:36:12 +0000

    alpha 1

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ var $$promise = require('./$$promise');
 
 exports.name = 'jpex-web';
 exports.install = function (options) {
-  var Jpex = options.Jpex;
+    var Jpex = options.Jpex;
 
-  if (!Jpex.$$factories.$promise){
-      var defaults = require('jpex-defaults');
-      Jpex.use(defaults);
-  }
+    if (!Jpex.$$factories.$promise){
+        var defaults = require('jpex-defaults');
+        Jpex.use(defaults);
+    }
 
-  Jpex.register.factory('$document', '$window', $document).lifecycle.application();
+    Jpex.register.factory('$document', '$window', $document).lifecycle.application();
 
-  Jpex.register.factory('$window', [], $window).lifecycle.application();
+    Jpex.register.factory('$window', [], $window).lifecycle.application();
 
-  Jpex.register.factory('$$promise', [], $$promise);
+    Jpex.register.factory('$$promise', [], $$promise);
 };
 
 if (typeof window !== 'undefined' && window.Jpex && typeof window.Jpex.use === 'function'){
